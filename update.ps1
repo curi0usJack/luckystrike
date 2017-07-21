caef1e28a9c9620a7b3b3d557524368ed01573ce
curi0usJack <curi0sJack@gmail.com>
2017-07-21 10:37:41 -0500

    Update repo location

--- update.ps1
+++ update.ps1
@@ -19,8 +19,8 @@ if (Test-Path -Path $currentdb)
 }
 
 Write-Output "[*] Downloading files"
-$db = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/Shellntel/luckystrike/master/db.sql')
-$ls = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/Shellntel/luckystrike/master/luckystrike.ps1')
+$db = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/Shellntel/curi0usJack/master/db.sql')
+$ls = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/Shellntel/curi0usJack/master/luckystrike.ps1')
 
 if ($db -eq $null -or $ls -eq $null)
 {
